d7a8786b15fb89ddb52a7f9b7b3665372ac0a7b2
Maxoscaro <svgerasimov92@yandex.ru>
2024-05-29 14:42:49 +0300

    Коммит перед заданием

---
+++ ImageFeed/ProfileViewController/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Maksim on 22.05.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBAction func didTapLogoutButton(_ sender: Any) {
+    }
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var ProfileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
+
